6b215d419e835d6e27b9c83300759db54e136dbb
Unknown <hej.mellor@gmail.com>
2018-01-03 22:43:12 +0000

    Converted sincinterp and reconstruc to vector methods

--- Reconstruction Optimise/reconstruct.m
+++ Reconstruction Optimise/reconstruct.m
@@ -3,21 +3,21 @@
 % Script to reconstruct func using sincinterp
 % DWM 15/9/11
 % Inputs:  f - frequency of signal
-%          ccoeff - multiple of f to cut off at
-%          scoeff - multiple of fm to sample at
+%          bf - multiple of f to bandlimit at
+%          sf - multiple of fm to sample at
 %
 % Outputs: I - integral of error squared
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function I = reconstruct(f,ccoeff,scoeff)
+function I = reconstruct(f,bf,sf)
 
-  fm     = ccoeff*f;          % chose frequency above f to cut off at
+  fm     = bf*f;          % chose frequency above f to cut off at
   wm     = 2*pi*fm;           % bandlimit in radians
   tstart = 0;                 % start time: arbitrary
   tstop  = 30/f;              % stop time: arbitrary
 
 % Choose sampling frequency
-  fs = scoeff*fm;             % close to the minimum allowed
+  fs = sf*fm;             % close to the minimum allowed
   ts = 1/fs;                  % Hence sampling period
 
 % Sample the function
@@ -29,13 +29,31 @@ function I = reconstruct(f,ccoeff,scoeff)
   tstep = ts/10;              % say 10 per orig sample
   tr = tstart:tstep:tstop;
   
-% Reconstruct at these times  % Ugly code warning
-  for n=1:length(tr)
-    fr(n) = sincinterp(tr(n),fsample,tsample,ts,wm,1,nsamples);
-  end;
+% Reconstruct at these times
+  fr = sincinterpv(tr,fsample,tsample,ts,wm);
   fo = func(tr,f);            % Find original func values
   err = fr-fo;                % Hence a vector of errors
   
 % Quantify error
   errsq = err.^2;             % Square error values so no negative error
   I = trapz(tr,errsq);
+  
+  figure()
+  hold off;
+  
+  subplot(3,1,1),plot(tr, fo, '-','LineWidth',2,'Color',[0 0 .7]);hold on;
+  subplot(3,1,1),plot(tsample, fsample,'O','LineWidth',2,'Color',[.7 0 0]);
+  title('Original function with samples')
+  xlabel('Time (s)')
+  ylabel('Amplitude')
+  set(gca,'FontName','Arial')
+  subplot(3,1,2),plot(tr, fr, '-','LineWidth',2,'Color',[0 0.7 0]);
+  title('Reconstructed function')
+  xlabel('Time (s)')
+  ylabel('Amplitude')
+  set(gca,'FontName','Arial')
+  subplot(3,1,3),plot(tr, err,'-','LineWidth',2,'Color',[0.6 0 0.6]);
+  title('Error')
+  xlabel('Time (s)')
+  ylabel('Amplitude')
+  set(gca,'FontName','Arial')
