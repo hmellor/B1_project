05f576a4100952623719bcac03779da1e6f7aa7e
Unknown <hej.mellor@gmail.com>
2018-01-13 10:13:37 +0000

    Completely reworked windowing

--- Reconstruction Optimise/sincinterpw.m
+++ Reconstruction Optimise/sincinterpw.m
@@ -7,8 +7,35 @@
 %         ts - vector of sample times
 %         Ts - sample period
 %         wm - bandlimit
+% Output: fr - reconstructed signal
 % UNFINISHED
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function fr = sincinterpw(wt,M,tr,fs,ts,Ts,wm)
+if wt == 1
+  k=(0:1:length(ts)-1)';    % k values of sum
+  p = (wm/pi)*(tr-k*Ts);    % columns - pk values for 1 summation
+  fr = Ts*wm/pi*fs*sinc(p); % reconstruc to single vector
+elseif wt == 2
+  fr = zeros(1,length(tr));          	    % preallocate fr
+  trm = 10*M;
+  for k=0:length(ts)-1                    % loop through each sample
+      trindex = (k+1)*10-9;    % centre index value, fix (round) becase can calculate as 0.00…1
+      upperindex = trindex + trm;
+      lowerindex = trindex - trm;     
+      if lowerindex < 1
+          lowerindex = 1;
+      end
+      if upperindex > length(tr)
+          upperindex = length(tr);
+      end
+      p = (wm/pi)*( tr(1, lowerindex: upperindex)-k*Ts);% vector of p values (width of window)
+      wsinc = Ts*wm/pi *fs(k+1)*sinc(p);                     % creates a vector which represents the sinc over tr
+      fr(1, lowerindex: upperindex) = fr(1, lowerindex: upperindex) + wsinc; 
+      % sums the sinc on windowed part of reconstructed curve
+  end
+end
+end
+%{
 function reconstruction = sincinterpw(wt,M,tr,fs,ts,Ts,wm)
   t=ones(length(ts),1);
   k = (0:1:length(ts)-1)';
@@ -46,3 +73,5 @@ function reconstruction = sincinterpw(wt,M,tr,fs,ts,Ts,wm)
       %}
   end
 end
+%}
+%}
--- Reconstruction Optimise/window_optimiser.m
+++ Reconstruction Optimise/window_optimiser.m
@@ -7,36 +7,41 @@
 f=1000;                                 % function frequency (arbitrary)
 sc=2.4;                                 % sample frequency coefficient
 bc=1.3;                                 % bandlimit frequency coefficient
-M=0:1:90;                               % sincinterp max window limit
-
+M=10:100:1100;                           % sincinterp max window limit
+r=3;                                    % repeats for timing
+run=0;                                  % resets run iterator
 % Initialise matrices and figure
-E=zeros(5,length(M));                   % preallocate error matrix
-T=zeros(5,length(M));                   % preallocate time matrix
+E=zeros(2,length(M));                   % preallocate error matrix
+T=zeros(2,length(M));                   % preallocate time matrix
 close all                               % close all figures
 figure()                                % open new figure
 grid on                                 % turn on grid for figure
 type = ["No window" "Rectangular window" "Linear window"...
       "Quadratic window" "Cosine window"]; % plot titles
+type = ["Matrix Method" "My window" " George window"...
+      "Quadratic window" "Cosine window"]; % plot titles
   
 % Evaluate calculation & and error for each window for range of cut offs
-for i = 1:5                             % iterate through each filter
+for i = 1:2                             % iterate through each filter
     for j = 1:length(M)                 % iterate through window limits
-        E(i,j) = reconstruct(f,bc,sc); % evaluate error
-        timer = @()reconstruct(f,bc,sc); % set handle with no args
-        T(i,j) = timeit(timer);         % measure time to reconstruct
+        j
+        E(i,j) = reconstructw(i,M(j),f,bc,sc); % evaluate error
+        timer = @()reconstructw(i,M(j),f,bc,sc); % set handle with no args
+        for run = 1:r
+            timed(run)=timeit(timer);
+        end
+        T(i,j) = median(timed);         % measure time to reconstruct
     end
-    % Plot the data
-    subplot(1,5,i)
-    hold off
-    yyaxis right
-    plot(M,T(i,:))
-    ylabel('Recontruction time')
-    axis([M(1) M(end) 0 inf])
-    hold on
-    yyaxis left
-    plot(M,E(i,:))
-    ylabel('Reconstruction error')
-    %axis([M(1) M(end) 0 inf])
-    title(type(i))
-    drawnow
 end
+% Plot the data
+hold on
+yyaxis right
+plot(M,T(1,:))
+plot(M,T(2,:))
+ylabel('Recontruction time')
+axis([M(1) M(end) 0 inf])
+yyaxis left
+plot(M,E(1,:))
+plot(M,E(2,:))
+ylabel('Reconstruction error')
+axis([M(1) M(end) 0 inf])
