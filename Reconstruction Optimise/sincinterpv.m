5769cc3512d878ea6aa139e584455f1e025d60cd
Unknown <hej.mellor@gmail.com>
2018-01-13 10:09:53 +0000

    Made Sincinterpv more concise

--- Reconstruction Optimise/sincinterpv.m
+++ Reconstruction Optimise/sincinterpv.m
@@ -5,10 +5,10 @@
 %         ts vector of sample times
 %         Ts sample period
 %         wm bandlimit
+% Outputs: I - integral of error squared
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function reconstruction = sincinterpv(tr,fs,ts,Ts,wm)
-  t=ones(length(ts),1)*tr; % meshgrid of ts and tr
-  k=(0:1:length(ts)-1)'*ones(1,length(tr)); 
-  p = (wm/pi)*(t-k*Ts);    % columns - pk values for 1 summation
-  reconstruction = Ts*wm/pi*fs*sinc(p); % reconstruc to single vector
+function fr = sincinterpv(tr,fs,ts,Ts,wm)
+  k=(0:1:length(ts)-1)';    % k values of sum
+  p = (wm/pi)*(tr-k*Ts);    % columns - pk values for 1 summation
+  fr = Ts*wm/pi*fs*sinc(p); % reconstruc to single vector
 end
