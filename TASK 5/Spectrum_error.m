4d51920c3a618843a03a0641138e44ee2436e71b
Unknown <hej.mellor@gmail.com>
2018-01-15 09:46:37 +0000

    Calculate error in the power of filtered spectra

--- TASK 5/Spectrum_error.m
+++ TASK 5/Spectrum_error.m
@@ -3,18 +3,21 @@ Pyyerr=Pyy(:,2:4);
 Pyyd=Pyy(:,1);
 Pyyd(89:end)=0;
 for i=1:3
-    Pyyerr(:,i)=abs(Pyyerr(:,i)-Pyyd);
-    Ierr(:,i)=trapz(Pyyerr(1:257,i));
-    I(:,i)=trapz(Pyy(1:88,1));
-    Iperc=Ierr./I;
+    %Pyyerr(:,i)=abs(Pyyerr(:,i)-Pyyd);
+    %Ierr(:,i)=trapz(Pyyerr(1:257,i));
+    %I(:,i)=simps(Pyy(1:88,1));
+    %Iperc=Ierr./I;
+    % Pass band
+    Ierr(:,i)=simps(Pyyerr(1:89,i))
+    I=simps(Pyy(1:89,1))
+    Iperc=100* Ierr./I    
     %{
-    for j=1:3
-        Pyyerr(:,j)=Pyyerr(:,j)./Pyy(:,1)
-    end
+    % Stop band
+    Ierr(:,i)=simps(Pyyerr(89:257,i))
+    I=simps(Pyy(89:257,1))
+    Iperc=100* Ierr./I
     %}
-    subplot(1,3,i);
-    plot(freq,Pyyerr(1:257,i));
 end
-I
-Ierr
-Iperc
+I;
+Ierr;
+Iperc;
